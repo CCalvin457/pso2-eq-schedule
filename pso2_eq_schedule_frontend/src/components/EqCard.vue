b12de59abfd7d3a4e16d85572f5541eca25d4093
Calvin Cheung <cheungcalvin2@gmail.com>
2020-08-24 21:51:49 -0400

    Added title tags to home route. Began templating how the eq card will look like.

---
+++ pso2_eq_schedule_frontend/src/components/EqCard.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-card class="mx-auto" max-width="500" max-height="300" shaped raised>
+        <v-card-title primary-title class="justify-center rm-bottom">
+            <p class="text-h4">Beach Wars! <!-- Eq name --></p>
+        </v-card-title>
+        <v-card-subtitle class="rm-bottom">
+            <p class="text-center text-h6">Urgent Quest <!-- Eq eventtype --></p>
+        </v-card-subtitle>
+        <v-card-text>
+            <v-container >
+                <v-row>
+                    <v-col cols="6">
+                        <div class="text-center text-body-1"><strong>Start Time:</strong></div>
+                        <p class="text-center text-body-1">12:00 AM PDT <!-- Eq time --></p>
+                    </v-col>
+                    <v-col cols="6">
+                        <div class="text-center text-body-1"><strong>Local Start Time:</strong></div>
+                        <p class="text-center text-body-1">3:00 AM EST <!-- Eq local time --></p>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12">
+                        <div class="text-center text-body-1"><strong>Duration:</strong></div>
+                        <p class="text-center text-body-1">30 Minutes <!-- Eq duration --></p>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.rm-bottom {
+    padding-bottom: 0;
+}
+</style>
--- pso2_eq_schedule_frontend/src/views/Home.vue
+++ pso2_eq_schedule_frontend/src/views/Home.vue
@@ -1,17 +1,34 @@
 <template>
   <div class="home">
     <DateInfo></DateInfo>
+    <v-container>
+      <v-row class="pb-4">
+        <v-col cols="12">
+          <p class="text-center text-h6">Upcoming Event:</p>
+          <EqCard></EqCard>
+        </v-col>
+      </v-row>
+      <v-divider></v-divider>
+      <v-row>
+        <v-col cols="12">
+          <p class="text-center text-h6">Today's Events:</p>
+          <EqCard></EqCard>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import DateInfo from '@/components/DateInfo.vue'
+import EqCard from '@/components/EqCard.vue'
 
 export default {
   name: 'Home',
   components: {
-    DateInfo
+    DateInfo,
+    EqCard
   }
 }
 </script>
