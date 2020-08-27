8a6a9806f6026081e8fd165a5d823b8d715a1714
Calvin Cheung <cheungcalvin2@gmail.com>
2020-08-27 15:57:45 -0400

    Prepared apps for hosting

--- pso2_eq_schedule_frontend/src/router/index.js
+++ pso2_eq_schedule_frontend/src/router/index.js
@@ -20,14 +20,6 @@ Vue.use(VueRouter)
     name: 'EqSchedule',
     component: EqSchedule
   }
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ]
 
 const router = new VueRouter({
