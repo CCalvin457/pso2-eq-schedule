a6aeac5e5ed9e82952eab6629fdb1a46ab9b4b71
Calvin Cheung <cheungcalvin2@gmail.com>
2020-09-30 15:46:45 -0400

    Update routes to first find the correct calendar id from the database before trying to grab calendar events

--- pso2_eq_schedule_backend/routes/index.js
+++ pso2_eq_schedule_backend/routes/index.js
@@ -3,5 +3,5 @@ const pso2CalendarRoute = require('./pso2_calendar');
 
 module.exports = (app, db, api) => {
     pso2EqsRoute(app, db);
-    pso2CalendarRoute(app, api);
+    pso2CalendarRoute(app, api, db);
 }
--- pso2_eq_schedule_backend/routes/pso2_calendar.js
+++ pso2_eq_schedule_backend/routes/pso2_calendar.js
@@ -1,23 +1,46 @@
-module.exports = (app, api) => {
-    app.get('/calendar', async (req, res) => {
-        const calendarId = 'pso2.schedule@gmail.com'
-        let events = []
-        let pageToken = undefined
+module.exports = (app, api, db) => {
+    // get latest calendar from google calendar
+    app.post('/calendar/latest', async (req, res) => {
+        let today = req.body.date;
+        today = new Date(today);
+        // console.log(typeof(today));
+        // let today = new Date();
+        let calendarId = '';
 
         try {
-            do {
-                /* eslint-disable no-await-in-loop */
-                let curEvents = await api.events.list({calendarId: calendarId, pageToken: pageToken})
-                if(curEvents.status != 200) {
-                    return res.status(curEvents.status).send(curEvents.statusText)
-                }
-                pageToken = curEvents.data.nextPageToken
+            let calendars = await db.collection('calendarIds').where('lastDate', '>=', today).orderBy('lastDate').get();
+            let data = calendars.docs.map(item => item.data());
+            // console.log(data);
+            calendarId = data[0].calendarId;
+        } catch(err) {
+            console.log(err);
+            return res.status(403).send("Something went wrong!");
+        }
+
+        return res.status(200).send(calendarId);
+    });
+
+    app.post('/calendar', async (req, res) => {
+        const calendarId = req.body.id;
+        // const calendarId = 'pso2.schedule@gmail.com'
+        let events = []
+        // let pageToken = undefined
+
+        try {
+            // do {
+                let curEvents = await api.events.list({calendarId: calendarId, orderBy: 'startTime', singleEvents: true});
+                // /* eslint-disable no-await-in-loop */
+                // let curEvents = await api.events.list({calendarId: calendarId, pageToken: pageToken})
+                // if(curEvents.status != 200) {
+                //     return res.status(curEvents.status).send(curEvents.statusText)
+                // }
+                // pageToken = curEvents.data.nextPageToken
 
                 curEvents.data.items.forEach(item => {
                     events.push(item)
                 });
 
-            } while(pageToken != undefined);
+            // } while(pageToken != undefined);
             
         } catch(err) {
             console.log(err)
